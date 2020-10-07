83eab573d26954b4cb36df174f3c80cabb1fb8c9
Joe Riley <joeyriley@gmail.com>
2020-10-07 00:51:02 -0700

    Went off the rails a bit here

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,42 +1,97 @@
+window.alert("Welcome to Robot Gladiators!");
 
 var playerName = window.prompt("What is your robot's name?");
-var playerHealth = 100;
-var playerAttack = 10;
 
-var getPlayerStats = () => {
-    return `Name: ${playerName}\nHealth: ${playerHealth}\nAttack: ${playerAttack}`;
-};
+var player = {
+    "name": playerName,
+    "health": 100,
+    "currentHealth": 100,
+    "attack": 10,
+    "money": 0
+}
 
-var enemyName = "JoeBerto";
-var enemyHealth = 50;
-var enemyAttack = 12;
-
-var getEnemyStats = () => {
-    return `Name: ${enemyName}\nHealth: ${enemyHealth}\nAttack: ${enemyAttack}`;
-};
-
-var fight = () => {
-    window.alert("Welcome to Robot Gladiators!");
-
-    enemyHealth -= playerAttack;
-    console.log(`${playerName} attacked ${enemyName}. ${enemyName} now has ${enemyHealth} health remaining.`)
-
-    if (enemyHealth <= 0) {
-        window.alert(`${enemyName} has died!`);
-    } 
-    else {
-        window.alert(`${enemyName} still has ${enemyHealth} health left.`);
+var enemies = [
+    {
+        "name": "Joeboto",
+        "health": 50,
+        "currentHealth": 50,
+        "attack": 12,
+        "prize": 5
+    },
+    {
+        "name": "Andy Android",
+        "health": 25,
+        "currentHealth": 25,
+        "attack": 16,
+        "prize": 7
+    },
+    {
+        "name": "BeeBop",
+        "health": 60,
+        "currentHealth": 60,
+        "attack": 9,
+        "prize": 4
     }
+];
 
-    playerHealth -= enemyAttack;
-    console.log(`${enemyName} attacked ${playerName}. ${playerName} now has ${playerHealth} health remaining.`)
+var randomRoll = () => {
+    return Math.random() < 0.5;
+};
 
-    if (playerHealth <= 0) {
-        window.alert(`${playerName} has died!`);
-    } 
-    else {
-        window.alert(`${playerName} still has ${playerHealth} health left.`);
+var choice = () => {
+    var promptFight = window.prompt("Would you likt to fight or skip this battle?\nEnter fight or skip to choose.");
+
+    if (promptFight.toLowerCase() === 'fight') {
+        // var names = [];
+        // window.prompt(`Choose a fighter. Your choices are:\n`)
+        fight(enemies[1]);
+    } else if (promptFight.toLowerCase() === 'skip') {
+        var confirmSkip = window.confirm("Are you sure you want to skip? This will cost you $2.00.");
+
+        if (confirmSkip) {
+            player['money'] -= 2;
+            window.alert(`${player['name']} has chosen to skip the fight! This has cost $2.00`);
+        } else {
+            choice();
+        }
+    } else {
+        window.alert("You need to pick a valid toption. Please try again.");
+        choice();
     }
 };
 
-fight();
+var attack = (attacker, target) => {
+    target.currentHealth -= Math.round(attacker['attack'] * Math.random());
+    console.log(`${attacker['name']} attacked ${target['name']}. ${target['name']} now has ${target.currentHealth} health remaining.`)
+};
+
+var flury = (enemy) => {
+    // Who attacks whom 50/50
+    if (randomRoll) {
+        attack(player, enemy);
+    } else {
+        attack(enemy, player);
+    };
+};
+
+var isKnockedOut = (fighter) => {
+    console.log(`Health of ${fighter['name']} = ${fighter['health']}`);
+    return fighter['health'] <= 0;
+};
+
+var fight = (enemy) => {
+    while (!isKnockedOut(player) && !isKnockedOut(enemy)) {
+        flury(enemy);
+    };
+
+    var winner = player['health'] > 0 ? player : enemy;
+
+    console.log(`${winner['name']} won the fight!`)
+
+    //reset fighters for next fight
+    player['currentHealth'] = player['health'];
+    enemy['currentHealth'] = enemy['health'];
+
+};
+
+choice();
